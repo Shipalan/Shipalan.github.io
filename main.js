0883ba79d85cfe35aae452d7fa98a37a5afedb6e
Shipalan <shipalan678@gmail.com>
2022-07-16 16:51:04 -0600

    Succeeded with return button functionality

--- main.js
+++ main.js
@@ -1,45 +1,35 @@
-const express = require("express")
-const cors = require("cors")
-const path = require("path")
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-let returnBtn = document.getElementById("returnBtn")
-
-
-app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "./index.html")) 
-})
-app.get("/css", (req,res) => {
-    res.sendFile(path.join(__dirname, "./portfolio.css")) 
-})
-app.get("/js", (req,res) => {
-    res.sendFile(path.join(__dirname, "./main.js")) 
-})
-app.get("/gitHub", (req,res) => {
-    res.sendFile(path.join(__dirname, "./pics/gitHub.png")) 
-})
-app.get("/linkedin", (req,res) => {
-    res.sendFile(path.join(__dirname, "./pics/linkedin.png")) 
-})
-app.get("/myself", (req,res) => {
-    res.sendFile(path.join(__dirname, "./pics/myself.png")) 
-})
-app.get("/upArrow", (req,res) => {
-    res.sendFile(path.join(__dirname, "./pics/upArrow.png")) 
-})
-app.get("/pointer", (req,res) => {
-    res.sendFile(path.join(__dirname, "./pics/pointer.png")) 
-})
-
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-
-returnBtn.app.listen("click", topFunction)
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+app.get("/css", (req, res) => {
+  res.sendFile(path.join(__dirname, "./portfolio.css"));
+});
+app.get("/js", (req, res) => {
+  res.sendFile(path.join(__dirname, "./main.js"));
+});
+app.get("/gitHub", (req, res) => {
+  res.sendFile(path.join(__dirname, "./pics/gitHub.png"));
+});
+app.get("/linkedin", (req, res) => {
+  res.sendFile(path.join(__dirname, "./pics/linkedin.png"));
+});
+app.get("/myself", (req, res) => {
+  res.sendFile(path.join(__dirname, "./pics/myself.png"));
+});
+app.get("/upArrow", (req, res) => {
+  res.sendFile(path.join(__dirname, "./pics/upArrow.png"));
+});
+app.get("/pointer", (req, res) => {
+  res.sendFile(path.join(__dirname, "./pics/pointer.png"));
+});
 
 const port = process.env.PORT || 3021;
 app.listen(port, () => console.log(`Listening on port ${port}`));
