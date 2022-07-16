868c8e74328845908f279718889849a8423d2680
Shipalan <shipalan678@gmail.com>
2022-07-15 20:56:57 -0600

    up1

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 // const express = require("express")
-// const path = require("path")
+const path = require("path")
 
 // const app = express();
 
 
-// app.get("/", (req,res) => {
-//     res.sendFile(path.join(__dirname, "./index.html")) 
-// })
-// app.get("css", (req,res) => {
-//     res.sendFile(path.join(__dirname, "./portfolio.css")) 
-// })
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "./index.html")) 
+})
+app.get("/css", (req,res) => {
+    res.sendFile(path.join(__dirname, "./portfolio.css")) 
+})
+app.get("/js", (req,res) => {
+    res.sendFile(path.join(__dirname, "./main.js")) 
+})
