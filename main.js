db6673d6672476be6eed219009fbba53ab3cc540
Shipalan <shipalan678@gmail.com>
2022-07-15 21:02:56 -0600

    up3

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const express = require("express")
+const cors = require("cors")
 const path = require("path")
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 
 app.get("/", (req,res) => {
